35b0d7e8ce4b9c1b44dcbe8736747cc9827d4799
scott.coleman <scott.coleman@tee2.org>
2014-06-27 15:00:01 -0700

    Added license

--- src/com/stc/FretboardView.java
+++ src/com/stc/FretboardView.java
@@ -1,3 +1,34 @@
+/*****************************************************************
+FretboardView
+
+Copyright (C) 2014 Scott T Coleman
+
+Eclipse Public License 1.0 (EPL-1.0)
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the Eclipse Public License as
+published by the Free Software Foundation, version 1.0 of the 
+License.
+
+The Eclipse Public License is a reciprocal license, under 
+Section 3. REQUIREMENTS iv) states that source code for the 
+Program is available from such Contributor, and informs licensees 
+how to obtain it in a reasonable manner on or through a medium 
+customarily used for software exchange.
+
+Post your updates and modifications to our GitHub or email to 
+scoleman2012@gmail.com.
+
+This library is distributed WITHOUT ANY WARRANTY; without 
+the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE.  See the Eclipse Public License 1.0 (EPL-1.0)
+for more details.
+ 
+You should have received a copy of the Eclipse Public License
+along with this library; if not, 
+visit http://www.opensource.org/licenses/EPL-1.0
+
+*****************************************************************/
 package com.stc;
 
 import java.util.Enumeration;
